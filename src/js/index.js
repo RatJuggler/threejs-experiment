92a605800c57a780d99675f215cfd100149b6751
John Chase <ratteal@gmail.com>
2019-09-18 22:26:00 +0100

    Add the basic spinning cube.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,39 @@
 require('../css/styles.css');
+const THREE = require('three');
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded', 'page-index')
-});
+var camera, scene, renderer, cube;
+
+init();
+animate();
+
+function init() {
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100 );
+    camera.position.z = 10;
+
+    scene = new THREE.Scene();
+    let geometry = new THREE.BoxGeometry( 4, 4, 4 );
+    let material = new THREE.MeshNormalMaterial();
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    document.body.appendChild( renderer.domElement );
+
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
