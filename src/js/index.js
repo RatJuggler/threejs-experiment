6c0041ddcdc66e117d65eb2c6ce754dc3e8efe84
John Chase <ratteal@gmail.com>
2019-09-19 10:29:23 +0100

    Add a ground plane and some comments.

--- src/js/index.js
+++ src/js/index.js
@@ -9,28 +9,42 @@ init();
 animate();
 
 function init() {
+    // Create camera.
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100 );
     camera.position.z = 10;
 
+    // Create scene.
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xe0e0e0 );
-    let geometry = new THREE.BoxGeometry( 4, 4, 4 );
-    let material = new THREE.MeshNormalMaterial();
-    cube = new THREE.Mesh( geometry, material );
+
+    // Create ground and add to scene.
+    let groundGeometry = new THREE.PlaneBufferGeometry( 200, 200 );
+    let groundMaterial = new THREE.MeshNormalMaterial();
+    let ground = new THREE.Mesh( groundGeometry, groundMaterial );
+    ground.rotation.x = - Math.PI / 2;
+    scene.add( ground );
+
+    // Create cube and add to scene.
+    let cubeGeometry = new THREE.BoxGeometry( 4, 4, 4 );
+    let cubeMaterial = new THREE.MeshNormalMaterial();
+    cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
     scene.add( cube );
 
+    // Create renderer.
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
 
+    // Add to page.
     document.body.appendChild( renderer.domElement );
 
+    // Create camera control.
     let controls = new OrbitControls( camera, renderer.domElement );
     controls.maxPolarAngle = Math.PI * 0.5;
     controls.minDistance = 10;
     controls.maxDistance = 50;
 
-    // Show stats.
+    // Create and add stats to page.
     stats = new Stats();
     document.body.appendChild( stats.dom );
 
