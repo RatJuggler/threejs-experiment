0b5358a3aa81fc651bdcd81cbdf36be1ec8b95dc
John Chase <ratteal@gmail.com>
2020-02-28 12:24:11 +0000

    Work on the grid plane.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import '../css/styles.css';
 import * as THREE from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
@@ -14,11 +16,16 @@ function init() {
     scene.background = new THREE.Color( 0xe0e0e0 );
 
     // Create ground and add to scene.
-    let groundGeometry = new THREE.PlaneBufferGeometry( 200, 200 );
-    let groundMaterial = new THREE.MeshNormalMaterial();
+    let groundGeometry = new THREE.PlaneBufferGeometry( 500, 500 );
+    let groundMaterial = new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } );
     let ground = new THREE.Mesh( groundGeometry, groundMaterial );
     ground.rotation.x = - Math.PI / 2;
     scene.add( ground );
+    // Make it a grid.
+    let grid = new THREE.GridHelper( 500, 100, 0x000000, 0x000000 );
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    scene.add( grid );
 
     // Create cube and add to scene above the ground.
     let cubeGeometry = new THREE.BoxGeometry( 4, 4, 4 );
@@ -45,6 +52,10 @@ function init() {
     controls.minDistance = 10;
     controls.maxDistance = 50;
 
+    let hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    hemiLight.position.set( 0, 100, 0 );
+    scene.add( hemiLight );
+
     // Create and add stats to page.
     stats = new Stats();
     document.body.appendChild( stats.dom );
@@ -60,8 +71,8 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame( animate );
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
     renderer.render( scene, camera );
     stats.update();
 }
